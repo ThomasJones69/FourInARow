096ca9ebfbb885df84dd6117511473db24c07372
ThomasJones69 <tjones@ziggo.nl>
2017-11-02 14:18:20 +0100

    Eerste opzet met CSS en JS

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+         <title></title>
+        <script src="javascriptbehavior.js"></script>
+        <link rel="stylesheet" href="StylingCascadeStyleSheet.css">        
+    </head>
+    <body>
+        <p onclick="test()">hello World</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
